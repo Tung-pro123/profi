1b685f676be8bacc46162531370bf9df4c4f5aa9
thanhtung <thanhtung22012006@ng22012006gmail.12006gmailcom>
2025-07-09 17:27:52 +0700

    Sửa reponsive và thêm, nhúng font chữ từ bên ngoài

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 particlesJS("particles-js", {
   particles: {
     number: {
-      value: 50,
+      value: 100,
       density: { enable: true, value_area: 800 }
     },
     color: { value: "#ffffff" },
